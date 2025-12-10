542bf09fde65aa76afc6638d1395f83c40a76140
amar-2008 <mm6155553@gmail.com>
2025-12-10 13:12:06 +0200

    Add new links and update background images

--- constants.ts
+++ constants.ts
@@ -3,22 +3,19 @@ export const DEVELOPER_NAME = "عمار مصطفى نوفل";
 export const DEVELOPER_PHONE = "201066052388"; 
 export const MEDICAL_LINK = "https://amar-medicine.vercel.app/"; 
 export const RELIGIOUS_LINK = "https://691c5f69b5903a8a2ccaaea8--ammar-islamic-azkar.netlify.app/";
+export const GEMINI_GEM_LINK = "https://gemini.google.com/gem/1wv3zGi5MxF4JYZYmvKAzMq_yM9hWVaD6?usp=sharing";
 export const DEV_EMAIL = "omamar152008@gmail.com";
 
 // --- EMAILJS CONFIGURATION ---
-// 1. Service ID
 export const EMAILJS_SERVICE_ID = "service_pcmxq0y"; 
-
-// 2. Template ID
 export const EMAILJS_TEMPLATE_ID = "template_kaqzzo4"; 
-
-// 3. Public Key
 export const EMAILJS_PUBLIC_KEY = "-EgajVw7NIziQUA9O";
 
 // Background Images
-export const BG_HOME = "https://i.ibb.co/8L3SBMpr/unnamed.jpg"; 
-export const BG_IMAGE_GEN = "https://i.ibb.co/SXcpDS9y/unnamed.jpg"; 
-export const BG_CHAT = "https://i.ibb.co/8n2vgsNn/unnamed.jpg"; 
+export const BG_HOME = "https://i.ibb.co/6R1K9dzs/f7d7b2b9-5dbf-4cd6-9be2-7d43c2573e9d.jpg"; 
+export const BG_CHAT = "https://i.ibb.co/My9wJSd8/f8d04d9c-6e77-4ccc-9fc5-4225a78366d7.jpg"; 
+export const BG_PROMPT = "https://i.ibb.co/Kz9gZmCd/fb0b4618-9384-4e64-b7c2-88831c3141ff.jpg";
+export const BG_CODING = "https://i.ibb.co/XrqVLfsj/45f4f78f-453c-4cd8-81a7-1a7443f3ab7c.jpg"; 
 
 // App Icon & Avatar
 export const APP_ICON_URL = "https://i.ibb.co/WWhngKKw/8b512605-7edb-4b1f-9f30-6e62396231f1.jpg";
